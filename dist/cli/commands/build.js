33aad220c7186b2a5c1b8c814e71e16c44211d24
iGalaxyYT <contact@igalaxy.dev>
2021-01-19 12:58:53 -0800

    Test build command

--- dist/cli/commands/build.js
+++ dist/cli/commands/build.js
@@ -9,11 +9,25 @@ exports.command = 'build';
 exports.desc = 'Build a TypeSkript project from the current directory';
 exports.builder = {};
 function handler(argv) {
+    // Just testing... not going to be the final thing, obviously.
+    var convert = function (ts) {
+        var lines = ts.split('\n');
+        var newLines = [];
+        var console_log_regex = /\'([^']+)\'/;
+        lines.forEach(function (line) {
+            line.split(';').slice(0, line.split(';').length - 1).forEach(function (subLine) {
+                if ((console_log_regex.exec(line) || [])[1] != undefined)
+                    newLines.push('log "' + (console_log_regex.exec(subLine) || [])[1] + '"');
+            });
+        });
+        var result = newLines.join('\n');
+        return result;
+    };
     !fs_1["default"].existsSync('./dist/') && fs_1["default"].mkdirSync('./dist/', { recursive: true });
     var copyDirectory = function (dir) {
         fs_1["default"].readdirSync("./src" + dir + "/", { withFileTypes: true }).forEach(function (file) {
             if (file.isFile())
-                fs_1["default"].writeFileSync(("./dist" + dir + "/" + file.name).replace('.ts', '.sk'), fs_1["default"].readFileSync("./src" + dir + "/" + file.name));
+                fs_1["default"].writeFileSync(("./dist" + dir + "/" + file.name).replace('.ts', '.sk'), convert(fs_1["default"].readFileSync("./src" + dir + "/" + file.name).toString()));
             if (file.isDirectory()) {
                 !fs_1["default"].existsSync("./dist" + dir + "/" + file.name) && fs_1["default"].mkdirSync("./dist" + dir + "/" + file.name, { recursive: true });
                 copyDirectory(dir + "/" + file.name);
--- src/cli/commands/build.ts
+++ src/cli/commands/build.ts
@@ -6,10 +6,29 @@ export const builder = {
 
 }
 export function handler(argv: any) {
+    // Just testing... not going to be the final thing, obviously.
+    
+    const convert = (ts: string): string => {
+        let lines = ts.split('\n');
+        let newLines: string[] = [];
+
+        const console_log_regex = /\'([^']+)\'/;
+
+        lines.forEach((line) => {
+            line.split(';').slice(0, line.split(';').length - 1).forEach(subLine => {
+                if((console_log_regex.exec(line) || [])[1] != undefined) newLines.push('log "' + (console_log_regex.exec(subLine) || [])[1] + '"');
+            });
+        });
+
+        let result = newLines.join('\n');
+
+        return result;
+    }
+
     !fs.existsSync('./dist/') && fs.mkdirSync('./dist/', { recursive: true });
     const copyDirectory = (dir: string): void => {
         fs.readdirSync(`./src${dir}/`, {withFileTypes: true}).forEach(file => {
-            if(file.isFile()) fs.writeFileSync(`./dist${dir}/${file.name}`.replace('.ts', '.sk'), fs.readFileSync(`./src${dir}/${file.name}`));
+            if(file.isFile()) fs.writeFileSync(`./dist${dir}/${file.name}`.replace('.ts', '.sk'), convert(fs.readFileSync(`./src${dir}/${file.name}`).toString()));
             if(file.isDirectory()) {
                 !fs.existsSync(`./dist${dir}/${file.name}`) && fs.mkdirSync(`./dist${dir}/${file.name}`, { recursive: true });
                 copyDirectory(`${dir}/${file.name}`);
